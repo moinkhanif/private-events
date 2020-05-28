4ca0cc4bec1cbdaac3b8afbfff5b73867ba7177a
Moin <kmoin786@gmail.com>
2020-05-29 04:10:11 +0530

    fix events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,17 +10,16 @@ class EventsController < ApplicationController
     @event = Event.new
   end
   def create
-    @event = Event.new(event_params)
-    @event.user_ids << current_user.id
+    @event = Event.new(name: params[:name],body: params[:body], date: params[:date], user_ids: current_user.id)
+    # @event.user_ids << current_user.id
     if @event.save
-      @user = @event.user_ids
-      redirect_to user_path(@user)
+      redirect_to user_path(current_user.id)
     else
       redirect_to new_events_path
     end
   end
 
   def event_params
-    params.require(:event)
+    
   end
 end
