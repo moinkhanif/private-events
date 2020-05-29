34fccd6e9a5d708b242fcc154390529d606d94ac
Moin <kmoin786@gmail.com>
2020-05-30 04:37:00 +0530

    fix routes linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :events
   end
   resources :events
-  resources :sessions, only: [:create, :new, :destroy]
+  resources :sessions, only: %i(:create :new :destroy)
   root to: 'users#index'
   get '/signup', to: 'users#new'
   get '/logout', to: 'sessions#destroy'
