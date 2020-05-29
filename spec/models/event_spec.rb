ac6aa06d90884688edc79e8c5c9097242ca7e969
Moin <kmoin786@gmail.com>
2020-05-30 04:31:29 +0530

    add model tests to rpec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "While creating new user" do
+    it "returns false if not valid" do
+      u = Event.new
+      expect(u.save).to eql(false)
+    end
+
+    it "returns true if valid entry" do
+      u = Event.create(name: "blah's event",body: "Blah's events body!",date: "2020-05-05 20:10:00")
+      expect(u.name).to eql("blah's event")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "While creating new user" do
+    it "returns false if not valid" do
+      u = User.new
+      expect(u.save).to eql(false)
+    end
+
+    it "returns true if valid entry" do
+      u = User.new(name: "blah")
+      expect(u.save).to eql(true)
+    end
+  end
+  
 end
