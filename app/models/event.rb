9211a8b4ac5bb601be816ff373f0b4e571cf67a4
Moin <kmoin786@gmail.com>
2020-06-01 01:23:21 +0530

    replace Time.now to Time.zone.now as suggested

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,6 @@ class Event < ApplicationRecord
   has_many :invites
   has_many :attendees, through: :invites, source: :user
 
-  scope :upcoming_date, -> { select { |event| event.date >= Time.now } }
-  scope :past_date, -> { select { |event| event.date < Time.now } }
+  scope :upcoming_date, -> { select { |event| event.date >= Time.zone.now } }
+  scope :past_date, -> { select { |event| event.date < Time.zone.now } }
 end
