93411b3d49d7f0f2c572b73a5a9a46b028f2b261
Moin <kmoin786@gmail.com>
2020-06-02 03:59:35 +0530

    optimize upcoming_events_action events view helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,26 +1,26 @@
 module EventsHelper
   def past_events_action
-    html = ''
     if @past_events.size > 0
-      @past_events.each do |event|
-        html += link_to event.name, event_path(event.id)
-      end
+      @past_events.map do |event|
+        link_to event.name, event_path(event.id)
+      end.join.html_safe
     else
-      html += "No past events found!"
+      content_tag(:p) do
+        "No past events found!"
+      end
     end
-    html.html_safe
   end
 
   def upcoming_events_action
-    html = ''
     if @upcoming_events.size > 0
-      @upcoming_events.each do |event|
-        html += link_to event.name, event_path(event.id)
-      end
+      @upcoming_events.map do |event|
+        link_to event.name, event_path(event.id)
+      end.join.html_safe
     else
-      html += "No upcoming! events found!"
+      content_tag(:p) do
+        "No upcoming! events found!"
+      end
     end
-    html.html_safe
   end
 
   def attendees_action
