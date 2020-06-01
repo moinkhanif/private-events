8a6f92668ecd34cf642e1d16cde918b2fa4c618d
Moin <kmoin786@gmail.com>
2020-06-02 04:13:14 +0530

    optimize user_helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,33 +1,25 @@
 module UsersHelper
   def user_invites_action
-    html = ''
-    @user_invites.each do |invite|
-      html += link_to Event.where(id: invite.event_id).first.name, event_path(invite.event_id)
-    end
-    html.html_safe
+    @user_invites.map { |invite| link_to Event.where(id: invite.event_id).first.name, event_path(invite.event_id) }.join.html_safe
   end
 
   def user_past_events_action
-    html = ''
-    if @user_past_events.size > 0
-      @user_past_events.each do |event|
-        html += link_to event.name, event_path(event.id)
-      end
+    if @user_past_events.size.positive?
+      @user_past_events.map { |event| link_to event.name, event_path(event.id) }.join.html_safe
     else
-      html += "No past events found!"
+      content_tag(:p) do
+        'No past events found!'
+      end
     end
-    html.html_safe
   end
 
   def user_upcoming_events_action
-    html = ''
-    if @user_upcoming_events.size > 0
-      @user_upcoming_events.each do |event|
-        html += link_to event.name, event_path(event.id)
-      end
+    if @user_upcoming_events.size.positive?
+      @user_upcoming_events.map { |event| link_to event.name, event_path(event.id) }.join.html_safe
     else
-      html += "No upcoming! events found!"
+      content_tag(:p) do
+        'No upcoming! events found!'
+      end
     end
-    html.html_safe
   end
 end
